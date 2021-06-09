c1a2d660e5ca522801b8cd8b1871ec8245e7638a
lakshmi <lakshmi31998@gmail.com>
2021-06-09 13:11:28 +0530

    make it better

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+
+
+const db = mysql.createConnection({
+
+    host: "us-cdbr-east-04.cleardb.com",
+    user: "b15e68aaaa3ba5",
+    password: "8df22ef2",
+    database: "heroku_0a6414aa002a7cc",
+    connectionLimit: 7,
+    dateStrings: true,
+    multipleStatements: true
+
+
+ 
+
+    });
+    
+module.exports = db;
+
+
