0f95f32f68e68e2437ef1a9c88ca756326aa8fce
lakshmi <lakshmi31998@gmail.com>
2021-06-10 20:17:57 +0530

    make it better

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors =require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT=process.env.PORT ;
+const PORT=process.env.PORT || 3001;
 const db = require('./config/db')
 
 db.connect((err) => {
