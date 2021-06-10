e3c9655673268cbac90c4c62a355fa8283e2176d
lakshmi <lakshmi31998@gmail.com>
2021-06-10 20:23:55 +0530

    make it better

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors =require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT=process.env.PORT || 3001;
+
 const db = require('./config/db')
 
 db.connect((err) => {
@@ -611,7 +611,7 @@ app.get("/projectreports",(req,res)=>{
       })
 
       
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
 
     console.log("Server started on port 3000");
     
